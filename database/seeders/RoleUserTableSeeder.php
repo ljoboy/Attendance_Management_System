96c8b5a61d07794b58dc697767653cc7d80f1027
Dark Angel <jonathanyombo@gmail.com>
2022-02-12 13:37:13 +0200

    [ADD] RoleUserTableSeeder.php

---
+++ database/seeders/RoleUserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class RoleUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::findOrFail(1)->roles()->sync(Role::findOrFail(1));
+    }
+}
