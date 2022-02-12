d954215a4d2900242b501afd49fbcdf25ecf5378
Dark Angel <jonathanyombo@gmail.com>
2022-02-12 13:42:37 +0200

    [UPDATE] seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(1)->create();
+        Employee::factory(20)->create();
+        $this->call([
+                        RoleTableSeeder::class,
+                        RoleUserTableSeeder::class
+                    ]);
     }
 }
