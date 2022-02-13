fde77054c4a0f809129479ba5de5cf1b9b25704e
Dark Angel <jonathanyombo@gmail.com>
2022-02-14 00:48:46 +0200

    [UPDATE] attendee by scanning

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Helpers\QrCodeHelper;
 use App\Models\Employee;
+use App\Services\AttendanceService;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Symfony\Component\HttpFoundation\Response;
 
 class QrCodeController extends Controller
@@ -46,5 +49,16 @@ class QrCodeController extends Controller
             ['success' => true, 'message' => "{$employee->name}'s QrCode exported successfully!"],
             Response::HTTP_CREATED
         );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function scan(string $encrypted_emp_id, AttendanceService $attendanceService): bool
+    {
+        $emp_id = Crypt::decrypt($encrypted_emp_id, true);
+        $employee = Employee::findOrFail($emp_id);
+
+        return $attendanceService->addAttendee($employee);
     }
 }
--- app/Models/Latetime.php
+++ app/Models/Latetime.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $emp_id
+ * @property string $duration
+ * @property string $latetime_date
+ */
 class Latetime extends Model
 {
-    public function employee()
+    public function employee(): BelongsTo
     {
         return $this->belongsTo(Employee::class, 'emp_id');
     }
