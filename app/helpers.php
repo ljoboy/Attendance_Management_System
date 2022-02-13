0b0c2aae08b3ed7ab72848c0da4e33374a9a09bb
Dark Angel <jonathanyombo@gmail.com>
2022-02-13 12:11:22 +0200

    [ADD] clean helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function flash($title=null, $message=null)
 {
@@ -9,5 +9,11 @@ function flash($title=null, $message=null)
     return $flash->info($title, $message);
 }
 
+function clean(string $string): array|string|null
+{
+    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+    $string = strtolower($string); // String to lowercase
 
-?>
+    return preg_replace('/-+/', '_', $string); // Replaces multiple hyphens with single one.
+}
