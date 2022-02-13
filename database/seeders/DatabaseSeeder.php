59fe9bfddad22efa57ae68ced305723150980526
Dark Angel <jonathanyombo@gmail.com>
2022-02-13 17:18:08 +0200

    [UPDATE] seed schedules

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Employee;
+use App\Models\Schedule;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(1)->create();
-        Employee::factory(20)->create();
+        Schedule::factory(2)->create();
+        Employee::factory(20)->create()->each(function (Employee $employee) {
+            $employee->schedules()->sync(Schedule::inRandomOrder()->first());
+        });
         $this->call([
                         RoleTableSeeder::class,
                         RoleUserTableSeeder::class
