e20122c42d14e2e40e59dfd581cfe4cb2cbbacfe
Dark Angel <jonathanyombo@gmail.com>
2022-02-13 12:09:10 +0200

    [UPDATE] refactor code

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -3,15 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property string $slug
+ * @property mixed $time_in
+ * @property mixed $time_out
+ */
 class Schedule extends Model
 {
-    public function getRouteKeyName()
+    protected $fillable = ['slug', 'time_in', 'time_out'];
+
+    public function getRouteKeyName(): string
     {
         return 'slug';
     }
 
-    public function employees()
+    public function employees(): BelongsToMany
     {
         return $this->belongsToMany('App\Models\Employee', 'schedule_employees', 'schedule_id', 'emp_id');
     }
