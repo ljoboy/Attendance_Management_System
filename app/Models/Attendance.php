21009394b0f253325c8f0932813f107bdc0c6103
Dark Angel <jonathanyombo@gmail.com>
2022-02-14 00:48:00 +0200

    [UPDATE] add attendable method

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 use App\Models\Employee;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @method static whereAttendance_date(string $date)
+ * @property int $emp_id
+ * @property string $attendance_date
+ * @property string $attendance_time
+ * @property bool $status
  */
 class Attendance extends Model
 
@@ -14,8 +19,13 @@ class Attendance extends Model
     protected $table = 'attendances';
 
 
-    public function employee()
+    public function employee(): BelongsTo
     {
-        return $this->belongsTo(Employee::class,'emp_id');
+        return $this->belongsTo(Employee::class, 'emp_id');
+    }
+
+    public static function attendable(string $date, int $id): ?Attendance
+    {
+        return self::whereAttendance_date($date)->whereEmp_id($id)->whereType(0)->first();
     }
 }
